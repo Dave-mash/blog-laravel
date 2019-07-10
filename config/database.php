309b41aeff17940c17c04bcc0498ccb5e91f285a
Dave <macharia3041@gmail.com>
2019-07-10 14:18:13 +0300

    modified database config

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'database' => $env ? env('DB_DATABASE') : env('ONLINE_DB_DATABASE'),
             'username' => $env ? env('DB_USERNAME') : env('ONLINE_DB_USERNAME'),
             'password' => $env ? env('DB_PASSWORD') : env('ONLINE_DB_PASSWORD'),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => env('DB_SOCKET', '/var/run/mysqld/mysqld.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
